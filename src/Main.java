5dd848f5ce19d5183c397e210d8162abfe1fdb02
cleslie <cjaleslie@gmail.com>
2014-09-15 13:35:19 +0100

    Deleted extra comments

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		MorseEncoder coder = new MorseEncoder();
 		coder.encodeInput();
+		
 	}
 
 }
